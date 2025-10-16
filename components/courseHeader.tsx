70b6372965d40e255ca8edb2ffe9c6a17ef378b6
nijilaep <nijilaep@gmail.com>
2025-10-16 23:52:14 +0530

    plane first phase completed

--- components/courseHeader.tsx
+++ components/courseHeader.tsx
@@ -8,7 +8,7 @@ export default function CourseHeader(data : any) {
       className="relative overflow-hidden w-full h-72 flex flex-col justify-center items-center text-center bg-gradient-to-r from-[#0197db] via-[#087fc2] to-[#0c8da6] px-8"
     >
       {/* absolute backgroundImage  */}
-      <img src="/logo_full.png" alt="" className='absolute -right-10 -99-9----999--+++++++9-9-9-96' />
+      <img src="/logo_full.png" alt="" className='absolute -right-10' />
 
         <h1 className='md:text-5xl text-3xl font-bold text-white mb-2'>{data?.data.header}</h1>
         {/* <p className='text-gray-100'>Expert coaching with quality study materials, regular tests, and <br />guidance for complete exam preparation.</p> */}
