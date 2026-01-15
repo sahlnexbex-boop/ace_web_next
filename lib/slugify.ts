d8e46718d550999a7db5b2686d3141bf47c673c8
sahlnexbex-boop <sahl.nexbex@gmail.com>
2026-01-15 14:37:14 +0530

    updated

--- lib/slugify.ts
+++ lib/slugify.ts
@@ -1,16 +1,16 @@
-export function slugify(text?: string) {
+export function slugify(text: any) {
   if (!text || typeof text !== "string") return "";
 
   return text
     .toLowerCase()
     .trim()
-    .replace(/[_\W]+/g, " ")
-    .trim()
+    .replace(/&/g, "and")
+    .replace(/[^a-z0-9\s]/g, "")
     .replace(/\s+/g, "-")
     .replace(/-+/g, "-");
 }
 
-export function deslugify(slug?: string) {
+export function deslugify(slug: any) {
   if (!slug) return "";
-  return slug.replace(/-/g, " ").toLowerCase();
+  return slug.replace(/-/g, " ");
 }
